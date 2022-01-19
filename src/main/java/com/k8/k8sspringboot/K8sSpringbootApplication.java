5059ae26f3f9171c5b754b034f5f704ff7ce0329
fengchao988 <chaofeng@ttyys.io>
2022-01-19 14:09:32 +0800

    init

---
+++ src/main/java/com/k8/k8sspringboot/K8sSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.k8.k8sspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8sSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(K8sSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/k8/k8sspringboot/K8sSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.k8.k8sspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
